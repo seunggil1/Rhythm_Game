f62f68b9ea9a8e111e3551a11b5e1341fc5793e9
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2019-06-02 00:32:48 +0900

    main menu script

---
+++ Assets/Scenes/mainMenu/script/music2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music2 : MonoBehaviour
+{
+    public main_mainMenu ob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        ob.music2.isClicked = true;
+    }
+}
