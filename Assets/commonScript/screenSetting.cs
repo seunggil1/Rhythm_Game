09b38a1089e17c7467f6b9d43a5bb2de98275875
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2019-06-02 00:23:39 +0900

    1920*1080 option added

--- Assets/commonScript/screenSetting.cs
+++ Assets/commonScript/screenSetting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class screenSetting : MonoBehaviour
 {
-    public void screenSet()
+    public void screenSet(int num = 1280)
     {
     
         Screen.orientation = ScreenOrientation.AutoRotation;
@@ -13,6 +13,9 @@ public class screenSetting : MonoBehaviour
         Screen.autorotateToLandscapeLeft = true;
         Screen.autorotateToLandscapeRight = true;
         
-        Screen.SetResolution(1280, 720, true);
+        if(num == 1920)
+            Screen.SetResolution(1920, 1080, true);
+        else
+            Screen.SetResolution(1280, 720, true);
     }
 }
