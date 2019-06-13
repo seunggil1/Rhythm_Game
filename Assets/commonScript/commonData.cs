4c51745c142aa26adc6ed37466bfb7cb5b71f6e4
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2019-06-14 02:42:34 +0900

    add maxCombo

--- Assets/commonScript/commonData.cs
+++ Assets/commonScript/commonData.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public static class commonData
 {
-    static public string path = "stage/1";
+    static public string path = "stage/2";
     static public string musicName = "music";
 
     static public string character = "";
     static public int Totalscore = 0;
     static public int score = 0;
     static public int combo = 0;
+    static public int maxCombo = 0;
 }
