c964d63d4bcf99bf26abf3dd17747ed43d488c97
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2019-06-15 00:11:38 +0900

    PlayerInfo read

--- Assets/Scenes/startScreen/script/main_startScreen.cs
+++ Assets/Scenes/startScreen/script/main_startScreen.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 
 public class main_startScreen : MonoBehaviour
 {
@@ -9,19 +9,25 @@ public class main_startScreen : MonoBehaviour
     void Start()
     {
         new screenSetting().screenSet();
+        TextAsset asset = Resources.Load("PlayerInfo") as TextAsset;
+        string data = asset.text;
+        StringReader dataReader = new StringReader(data);
 
+        commonData.character = dataReader.ReadLine();
+        commonData.Totalscore = int.Parse(dataReader.ReadLine());
+        commonData.maxCombo = int.Parse(dataReader.ReadLine());
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Application.platform == RuntimePlatform.Android)
-            if (Input.GetKey(KeyCode.Escape))
-                Application.Quit();
+        //if (Application.platform == RuntimePlatform.Android)
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
     }
     void FixedUpdate()
     {
-        if(Time.time > 1.5)
+        if(Time.time > 1)
             if (Input.touchCount > 0)
                 UnityEngine.SceneManagement.SceneManager.LoadScene("mainMenu");
     }
