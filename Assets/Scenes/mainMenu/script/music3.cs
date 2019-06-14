f3e6dcacc22dd76248a9bd5972fd855eac2fff99
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2019-06-15 00:09:38 +0900

    music3 button activiated

---
+++ Assets/Scenes/mainMenu/script/music3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music3 : MonoBehaviour
+{
+    public main_mainMenu ob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        ob.music3.isClicked = true;
+    }
+}
