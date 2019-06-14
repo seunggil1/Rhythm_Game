0fcc515435ded8c5baa513d0fa7b2ff25eb5c1b2
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2019-06-14 23:46:17 +0900

    comment add about must save data

--- Assets/commonScript/commonData.cs
+++ Assets/commonScript/commonData.cs
@@ -7,9 +7,9 @@ public static class commonData
     static public string path = "stage/2";
     static public string musicName = "music";
 
-    static public string character = "";
-    static public int Totalscore = 0;
+    static public string character = "character1"; //save
+    static public int Totalscore = 0; //save
     static public int score = 0;
     static public int combo = 0;
-    static public int maxCombo = 0;
+    static public int maxCombo = 0; // save
 }
